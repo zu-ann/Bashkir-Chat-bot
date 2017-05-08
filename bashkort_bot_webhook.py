a122e75b38c513c787d0d546520f6800348d2298
zu-ann <asl29112@gmail.com>
2017-05-09 00:10:11 +0300

    Update and rename bashkort_bot.py to bashkort_bot_webhook.py

--- bashkort_bot_webhook.py
+++ bashkort_bot_webhook.py
@@ -1,30 +1,44 @@
-import telebot, re, conf, json, random, time, translate #flask,
+# -*- coding: utf-8 -*-
+import telebot, os, re, conf, json, random, time, translate
+from flask import Flask, request
+import tensorflow as tf
 
-#WEBHOOK_URL_BASE = "https://{}:{}".format(conf.WEBHOOK_HOST, conf.WEBHOOK_PORT)
-#WEBHOOK_URL_PATH = "/{}/".format(conf.TOKEN)
+WEBHOOK_HOST = 'bashkort-bot.herokuapp.com'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '162.243.19.215'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = 'webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'  # Path to the ssl private key
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (conf.TOKEN)
+
+bot.remove_webhook()
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH, certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
 bot = telebot.TeleBot(conf.TOKEN, threaded=False)
 
-# удаляем предыдущие вебхуки, если они были
-#bot.remove_webhook()
+app = Flask(__name__)
 
-# ставим новый вебхук = Слышь, если кто мне напишет, стукни сюда — url
-#bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
+sess = tf.Session()
+sess, model, en_vocab, rev_fr_vocab = translate.start(sess)
+print('created sess')
+regUNK = re.compile('_UNK')
 
-#app = flask.Flask(__name__)
-
-# этот обработчик запускает функцию send_welcome, когда пользователь отправляет команды /start или /help
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.send_message(message.chat.id, "Здравствуйте! Это бот на башкирском.")
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def send_sample_answers(message):
     text = message.text
+    answer = 0
     f1 = open('Sample_answers.json','r',encoding = 'UTF-8')
     f2 = open('regex.json','r',encoding = 'UTF-8')
     sample_answers = json.load(f1)
     regex = json.load(f2)
+    f1.close()
+    f2.close()
     for lst in regex:
         for elem in regex[lst]:
             res = re.search(elem,text)
@@ -50,27 +64,14 @@ def send_sample_answers(message):
                         sample_answers[lst].append('Хәйерле төн! (Доброй ночи)')
                         print('Хәйерле төн! (Доброй ночи)')
                 bot.send_message(message.chat.id, random.choice(sample_answers[lst]))
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def send_answer_by_seq2seq(message):
-   bot.send_message(message.chat.id, translate.decode_for_bot(message.text))
-
-# пустая главная страничка для проверки
-#@app.route('/', methods=['GET', 'HEAD'])
-#def index():
-#    return 'ok'
-
-
-# обрабатываем вызовы вебхука = функция, которая запускается, когда к нам постучался телеграм
-#@app.route(WEBHOOK_URL_PATH, methods=['POST'])
-#def webhook():
-#    if flask.request.headers.get('content-type') == 'application/json':
-#        json_string = flask.request.get_data().decode('utf-8')
-#        update = telebot.types.Update.de_json(json_string)
-#        bot.process_new_updates([update])
-#        return ''
-#    else:
-#        flask.abort(403)
-
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+                answer = 1
+    if answer == 0:
+        print('use send_answer_by_seq2seq')
+        reply = translate.decode_for_bot(sess, model, en_vocab, rev_fr_vocab, text)
+        reply = regUNK.sub('Ғәфү итегеҙ, мин аңламайым.',reply)
+        bot.send_message(message.chat.id, reply)
+                   
+app.run(host=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT,
+        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+        debug=True)

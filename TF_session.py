d5492f566152912e2d222e0bbfddd87cfd82c1e4
zu-ann <asl29112@gmail.com>
2017-05-18 11:17:11 +0300

    Update TF_session.py

--- TF_session.py
+++ TF_session.py
@@ -53,41 +53,42 @@ def create_model(session, forward_only):
   print("created sess")
   return model
 
+def TF_session(sentence):
+    with tf.Session() as sess:
+        # Create model and load parameters.
+        model = create_model(sess, True)
+        model.batch_size = 1  # We decode one sentence at a time.
+
+        # Load vocabularies.
+        en_vocab_path = os.path.join(data_dir,
+                                     "vocab%d.en" % en_vocab_size)
+        fr_vocab_path = os.path.join(data_dir,
+                                     "vocab%d.fr" % fr_vocab_size)
+        en_vocab, _ = data_utils.initialize_vocabulary(en_vocab_path)
+        _, rev_fr_vocab = data_utils.initialize_vocabulary(fr_vocab_path)
+        while sentence:
+            # Get token-ids for the input sentence.
+            token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), en_vocab)
+            # Which bucket does it belong to?
+            bucket_id = min([b for b in xrange(len(_buckets))
+                             if _buckets[b][0] > len(token_ids)])
+            # Get a 1-element batch to feed the sentence to the model.
+            encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+                {bucket_id: [(token_ids, [])]}, bucket_id)
+            # Get output logits for the sentence.
+            _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs,
+                                             target_weights, bucket_id, True)
+            # This is a greedy decoder - outputs are just argmaxes of output_logits.
+            outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+            # If there is an EOS symbol in outputs, cut them at that point.
+            if data_utils.EOS_ID in outputs:
+                outputs = outputs[:outputs.index(data_utils.EOS_ID)]
+            return " ".join([tf.compat.as_str(rev_fr_vocab[output]) for output in outputs]), sess
+
 def answer_by_seq2seq(sentence):
     print('use send_answer_by_seq2seq')
     regUNK = re.compile('_UNK')
-    sess = tf.Session()
-    # Create model and load parameters.
-    model = create_model(sess, True)
-    model.batch_size = 1  # We decode one sentence at a time.
-
-    # Load vocabularies.
-    en_vocab_path = os.path.join(data_dir,
-                                     "vocab%d.en" % en_vocab_size)
-    fr_vocab_path = os.path.join(data_dir,
-                                     "vocab%d.fr" % fr_vocab_size)
-    en_vocab, _ = data_utils.initialize_vocabulary(en_vocab_path)
-    _, rev_fr_vocab = data_utils.initialize_vocabulary(fr_vocab_path)
-    print ("prepared vocab")
-#    while sentence:
-    print("started sentence")
-            # Get token-ids for the input sentence.
-    token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), en_vocab)
-            # Which bucket does it belong to?
-    bucket_id = min([b for b in xrange(len(_buckets))
-                             if _buckets[b][0] > len(token_ids)])
-            # Get a 1-element batch to feed the sentence to the model.
-    encoder_inputs, decoder_inputs, target_weights = model.get_batch(
-                {bucket_id: [(token_ids, [])]}, bucket_id)
-            # Get output logits for the sentence.
-    _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs,
-                                             target_weights, bucket_id, True)
-            # This is a greedy decoder - outputs are just argmaxes of output_logits.
-    outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
-            # If there is an EOS symbol in outputs, cut them at that point.
-    if data_utils.EOS_ID in outputs:
-        outputs = outputs[:outputs.index(data_utils.EOS_ID)]
-    reply = " ".join([tf.compat.as_str(rev_fr_vocab[output]) for output in outputs])
+    reply, sess = TF_session(sentence)
     print(reply)
     del sess
     reply = regUNK.sub('Ғәфү итегеҙ, мин аңламайым.',reply)

53d1f0d89eb49bfb77c00763387f9f5eeca652db
zu-ann <asl29112@gmail.com>
2017-05-29 02:31:17 +0300

    phrases

--- phrases.py
+++ phrases.py
@@ -4,8 +4,8 @@ List all phrases that a bot can say to user.
 
 start = 'Cәләм! Мин башҡорт телендә һөйләшә белгән беренсе бот, әйҙә аралашайыҡ!'
 
-help = 'This bot can carry on a dialogue using the Bashkir language.\n Supported commands:\n /start - start the conversation\n /help - about the bot\n /mark - give a mark to the quality of the bot\'s answers\n\n If you want to get more information about this bot – please visit https://github.com/zu-ann/Bashkort_chatbot'
+help = 'This bot can carry on a dialogue using the Bashkir language.\n\n The supported commands are:\n /start - start the conversation\n /help - about the bot\n /mark - give a mark to the quality of the bot\'s answers\n\n If you want to get more information about this bot – please visit https://github.com/zu-ann/Bashkort_chatbot'
 
 wrong_language = 'Sorry, please speak Bashkir.'
 
-mark = "Пожалуйста, оцените ответ чат-бота:\n 👎 - ответ чат-бота никак не связан с Вашим сообщением\n 😐 - ответ чат-бота можно рассматривать как ответ на Ваше сообщение, но лишь косвенно\n 👍 - хороший ответ на Ваше сообщение"
+mark = "Please, give a mark to the bot's answer:\n\n 👎 - the answer consists only of incoherent words\n 😐 - the answer seems to be connected to your message, but only obliquely\n 👍 - a good answer to your message"
